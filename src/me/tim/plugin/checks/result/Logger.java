e346f9eac6bdd2e476de91b3103e1536a750fa67
Web-Tim <teichmanntim@outlook.de>
2021-06-14 14:57:02 +0200

    First Commit

--- src/me/tim/plugin/checks/result/Logger.java
+++ src/me/tim/plugin/checks/result/Logger.java
@@ -1,8 +1,12 @@
 package me.tim.plugin.checks.result;
 
+import org.bukkit.Bukkit;
+
 public class Logger {
 
     public static void logCheck(CheckResult r) {
+        Bukkit.getOnlinePlayers().forEach(player -> {
 
+        });
     }
 }
